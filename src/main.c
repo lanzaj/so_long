c4842f9841b0bc756e951b03bfd9ccd3e09b1fa5
Joseph LANZA <jlanza@student.42.fr>
2024-04-23 17:28:26 +0200

    enable to launch game without map

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jlanza <jlanza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 15:05:56 by jlanza            #+#    #+#             */
-/*   Updated: 2023/02/08 11:36:57 by jlanza           ###   ########.fr       */
+/*   Updated: 2024/04/23 17:27:14 by jlanza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@ int	main(int argc, char *argv[])
 {
 	t_data	data;
 
-	if (argc != 2)
+	if (argc > 2)
 		parse_map_error(1);
-	data.map.ptr = import_map(argv[1]);
+	if (argc == 2)
+		data.map.ptr = import_map(argv[1]);
+	else
+		data.map.ptr = import_map("./maps/valid/big_map.ber");
 	check_map(data.map.ptr);
 	init_data(&data);
 	game_init(&data);
